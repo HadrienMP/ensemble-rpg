aed0dfc2e2018ac8da5551813d704865e40d4e41
Hadrien Mens-Pellen <hadrien.menspellen@sundayapp.com>
2022-01-30 15:51:28 +0100

    attempt to serve from heroku

---
+++ server/app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const path = require('path');
+
+
+let publicDirPath = path.join(__dirname + "/../", 'public');
+app.use(express.static(publicDirPath));
+app.get('/', (req, res) => {
+    res.sendFile(join(publicDirPath, "index.html"))
+})
+
+const port = process.env.PORT || 3000
+app.listen(port, () => {
+    console.log(`Live at http://0.0.0.0:${port}`)
+});
